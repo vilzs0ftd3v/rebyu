a4f0d32b6c85da6844a7035b1ebcefded3b72db8
vilzs0ftd3v <68156648+vilzs0ftd3v@users.noreply.github.com>
2020-12-28 16:40:32 +0800

    Update base.php

--- data/home/base.php
+++ data/home/base.php
@@ -9,7 +9,8 @@ class Database extends PDO
 	function __construct()
 	{
 		//parent::__construct("mysql:host=sql201.0fees.us;dbname=0fe_23763878_review;","0fe_23763878","RheaS0ftd3v");
-		parent::__construct("mysql:host=myelination;dbname=myelinat_rebyu;","myelinat_admin","#Rheas0ftd3v#");
+		//parent::__construct("mysql:host=myelination;dbname=myelinat_rebyu;","myelinat_admin","#Rheas0ftd3v#");
+		parent::__construct("mysql:host=freedb.tech;dbname=freedbtech_myelination","freedbtech_username","password");
 	}
 
 
